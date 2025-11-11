d5893dc557d36efc4cb38e2bebcef53db0f7ad7d
jjlindsey <jlindsey010@gmail.com>
2025-11-11 11:39:12 -0800

    setup routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import VanGoghData from '@/views/VanGoghData.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {path: '/', name: 'homeview', component: HomeView},
+    {path: '/data',name: 'data', component:VanGoghData}
+  ],
 })
 
 export default router

754c8f6e58987fee6e6ca8edafe2883228c35ae5
jjlindsey <jlindsey010@gmail.com>
2025-11-18 13:11:24 -0800

    fix merge conflict

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+//import HomeView from '@/views/HomeView.vue'
 import VanGoghData from '@/views/VanGoghData.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {path: '/', name: 'homeview', component: HomeView},
-    {path: '/data',name: 'data', component:VanGoghData}
+    // {path: '/', name: 'homeview', component: HomeView},
+    {path: '/data', name: 'data', component:VanGoghData},
+    {path: '/', name: 'homeview', component: VanGoghData},
   ],
 })
 

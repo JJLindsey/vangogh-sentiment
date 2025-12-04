a94bbbd18c6e5a0b3731adc61d4d35d79806151b
jjlindsey <jlindsey010@gmail.com>
2025-12-04 10:38:47 -0800

    update theme colours and revise naming

--- src/views/VanGoghData.vue
+++ src/views/VanGoghData.vue
@@ -68,7 +68,7 @@
         <!-- <div class="metrics-grid"> -->
           <div class="metric-card">
             <div class="metric-header">
-              <div class="metric-icon metric-icon-blue">
+              <div class="metric-icon metric-icon-sentiment">
                 <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"/>
                 </svg>
@@ -81,7 +81,7 @@
 
           <div class="metric-card">
             <div class="metric-header">
-              <div class="metric-icon metric-icon-teal">
+              <div class="metric-icon metric-icon-academic">
                 <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253"/>
                 </svg>
@@ -94,7 +94,7 @@
           <!-- Brand card-->
           <div class="metric-card">
             <div class="metric-header">
-              <div class="metric-icon metric-icon-accent">
+              <div class="metric-icon metric-icon-brand">
                 <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"/>
                 </svg>
@@ -107,7 +107,7 @@
 
           <div class="metric-card">
             <div class="metric-header">
-              <div class="metric-icon metric-icon-gold">
+              <div class="metric-icon metric-icon-publications">
                 <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"/>
                 </svg>
@@ -176,6 +176,7 @@
                   </div>
                 </div>
               </div>
+
                <!-- Commercialization Section -->
               <div class="content-card commercialization-card">
                 <h3 class="card-title">Commercialization as Cultural Indicator</h3>
@@ -204,6 +205,15 @@
                   </div>
                 </div>
               </div>
+              <!--Time Line-->
+              <div class="card-contnet">
+                <Timeline />
+              </div>
+               <!-- Brand Logos-->
+               <div class="content-card">
+                <LogoGrid />
+               </div>
+
             </section>
           </div>
 
@@ -425,13 +435,17 @@ import SentimentLineChart from '@/components/SentimentLineChart.vue';
 //import HistoricalReceptionChart from '@/components/HistoricalReceptionChart.vue';
 import ComparisonChart from '@/components/ComparisonChart.vue';
 import BrandCollaborationChart from '@/components/BrandCollaborationChart.vue';
+import LogoGrid from '@/components/LogoGrid.vue';
+import Timeline from '@/components/Timeline.vue';
 
 export default {
   components: {
     SentimentLineChart,
     //HistoricalReceptionChart,
     ComparisonChart,
-    BrandCollaborationChart
+    BrandCollaborationChart,
+    Timeline,
+    LogoGrid,
   },
   data() {
     return {
